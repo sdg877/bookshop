4835b80db6a87fd921b552da8872d5011edec3a4
Sdg877 <sdrakegill@gmail.com>
2024-08-20 08:30:47 +0100

    post request for books added

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import { PORT } from "./config.js";
+import { Book } from './models/bookModel.js';
 
 const app = express();
 dotenv.config();
@@ -11,6 +12,31 @@ console.log("Database URL:", DATABASE_URL);
 app.get("/", (request, response) => {
   console.log(request);
   return response.status(200).send("Welcome to MERN stack");
+});
+
+app.post('/books', async (request, response) => {
+    try {
+        if (
+            !request.body.title ||
+            !request.body.author ||
+            !request.body.publishYear
+        ) {
+            return response.status(400).send({
+                message: 'Send all required fields',
+            });
+        }
+        const newBook = {
+            title: request.body.title,
+            author: request.body.author,
+            publishYear: request.body.publishYear,
+        };
+        const book = await Book.create(newBook);
+
+        return response.status(201).send(book);
+    } catch (error) {
+        console.log(error.message);
+        response.status(500).send({ message: error.message })
+    }
 });
 
 app.listen(PORT, () => {
