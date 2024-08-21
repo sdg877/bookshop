b3d5b23cdb9bedd0b7904885052fde190c77ca30
Sdg877 <sdrakegill@gmail.com>
2024-08-21 14:07:41 +0100

    routes for finding single book and updating book added

--- backend/index.js
+++ backend/index.js
@@ -59,7 +59,7 @@
 import express, { response } from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
-import { Book } from './models/bookModel.js';
+import { Book } from "./models/bookModel.js";
 
 dotenv.config();
 
@@ -77,37 +77,78 @@ app.get("/", (request, response) => {
   return response.status(200).send("Welcome to MERN stack");
 });
 
-app.post('/books', async (request, response) => {
-    try {
-        if (
-            !request.body.title ||
-            !request.body.author ||
-            !request.body.publishYear
-        ) {
-            return response.status(400).send({
-                message: 'Send all required fields',
-            });
-        }
-        const newBook = {
-            title: request.body.title,
-            author: request.body.author,
-            publishYear: request.body.publishYear,
-        };
-        const book = await Book.create(newBook);
-
-        return response.status(201).send(book);
-    } catch (error) {
-        console.log(error.message);
-        response.status(500).send({ message: error.message })
+app.post("/books", async (request, response) => {
+  try {
+    if (
+      !request.body.title ||
+      !request.body.author ||
+      !request.body.publishYear
+    ) {
+      return response.status(400).send({
+        message: "Send all required fields",
+      });
     }
+    const newBook = {
+      title: request.body.title,
+      author: request.body.author,
+      publishYear: request.body.publishYear,
+    };
+    const book = await Book.create(newBook);
+
+    return response.status(201).send(book);
+  } catch (error) {
+    console.log(error.message);
+    response.status(500).send({ message: error.message });
+  }
 });
 
-app.get('/books', async (request, response) => {
+app.get("/books", async (request, response) => {
   try {
     const books = await Book.find({});
-    return response.status(200).json(books);
+    return response.status(200).json({
+      count: books.length,
+      data: books,
+    });
   } catch (error) {
-    console.log (error.message);
+    console.log(error.message);
+    response.status(500).send({ message: error.message });
+  }
+});
+
+app.get("/books/:id", async (request, response) => {
+  try {
+    const { id } = request.params;
+
+    const book = await Book.findById(id);
+
+    return response.status(200).json(book);
+  } catch (error) {
+    console.log(error.message);
+    response.status(500).send({ message: error.message });
+  }
+});
+
+app.put("/books/:id", async (request, response) => {
+  try {
+    if (
+      !request.body.title ||
+      !request.body.author ||
+      !request.body.publishYear
+    ) {
+      return response.status(400).send({
+        message: 'Send all required feilds: title, author, publish year'
+      })
+    }
+
+    const { id } = request.params;
+
+    const result = await Book.findByIdAndUpdate(id, request.body);
+
+    if (!result) {
+      return response.status(404).json({ message: 'Book not found! '})
+    }
+  } catch (error) {
+    console.log(error.message);
     response.status(500).send({ message: error.message });
   }
 });
