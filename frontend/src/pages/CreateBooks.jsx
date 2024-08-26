71c4b7530ba8016e958d56cfe5fff916e45370b7
Sdg877 <sdrakegill@gmail.com>
2024-08-26 08:13:08 +0100

    edit book page implememnted

--- frontend/src/pages/CreateBooks.jsx
+++ frontend/src/pages/CreateBooks.jsx
@@ -35,7 +35,7 @@ const CreateBooks = () => {
       <BackButton />
       <h1 className="text-3xl my-4">Create Book</h1>
       {loading ? <Spinner /> : ""}
-      <div className="flex flex-col border-2 border-sky-400 rounded-xl w-[600px] p-4 mx auto"></div>
+      <div className="flex flex-col border-2 border-sky-400 rounded-xl w-[600px] p-4 mx auto">
       <div className="my-4">
         <label className="text-xl mr-4 text-gray-500">Title</label>
         <input
@@ -66,6 +66,7 @@ const CreateBooks = () => {
       <button className="p-2 bg-sky-300 m-8" onClick={handleSaveBook}>
         Save
       </button>
+    </div>
     </div>
   );
 };
