1b0040c94c44c2c2ab14e77f2050056412722a7c
Sdg877 <sdrakegill@gmail.com>
2024-08-29 14:04:15 +0100

    snackbar added for create, delete and edit. project completed.

--- frontend/src/pages/CreateBooks.jsx
+++ frontend/src/pages/CreateBooks.jsx
@@ -1,9 +1,9 @@
 import React, { useState } from "react";
-import { useNavigate } from 'react-router-dom';
 import BackButton from "../components/BackButton";
 import Spinner from "../components/Spinner";
 import axios from "axios";
-import { useSnackbar } from 'notistack';
+import { useNavigate } from "react-router-dom";
+import { useSnackbar } from "notistack";
 
 const CreateBooks = () => {
   const [title, setTitle] = useState("");
@@ -11,6 +11,8 @@ const CreateBooks = () => {
   const [publishYear, setPublishYear] = useState("");
   const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
+  const { enqueueSnackbar } = useSnackbar();
+
   const handleSaveBook = () => {
     const data = {
       title,
@@ -22,11 +24,12 @@ const CreateBooks = () => {
       .post("http://localhost:5555/books", data)
       .then(() => {
         setLoading(false);
+        enqueueSnackbar("Book Created successfully", { variant: "success" });
         navigate("/");
       })
       .catch((error) => {
         setLoading(false);
-        alert("An error happened, please check console");
+        enqueueSnackbar("Error", { variant: "error" });
         console.log(error);
       });
   };
@@ -36,38 +39,38 @@ const CreateBooks = () => {
       <BackButton />
       <h1 className="text-3xl my-4">Create Book</h1>
       {loading ? <Spinner /> : ""}
-      <div className="flex flex-col border-2 border-sky-400 rounded-xl w-[600px] p-4 mx auto">
-      <div className="my-4">
-        <label className="text-xl mr-4 text-gray-500">Title</label>
-        <input
-          type="text"
-          value={title}
-          onChange={(e) => setTitle(e.target.value)}
-          className="border-2 border-gray-500 px-4 py-2 w-full"
-        />
+      <div className="flex flex-col border-2 border-sky-400 rounded-xl w-[600px] p-4 mx-auto">
+        <div className="my-4">
+          <label className="text-xl mr-4 text-gray-500">Title</label>
+          <input
+            type="text"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            className="border-2 border-gray-500 px-4 py-2 w-full"
+          />
+        </div>
+        <div className="my-4">
+          <label className="text-xl mr-4 text-gray-500">Author</label>
+          <input
+            type="text"
+            value={author}
+            onChange={(e) => setAuthor(e.target.value)}
+            className="border-2 border-gray-500 px-4 py-2  w-full "
+          />
+        </div>
+        <div className="my-4">
+          <label className="text-xl mr-4 text-gray-500">Publish Year</label>
+          <input
+            type="number"
+            value={publishYear}
+            onChange={(e) => setPublishYear(e.target.value)}
+            className="border-2 border-gray-500 px-4 py-2  w-full "
+          />
+        </div>
+        <button className="p-2 bg-sky-300 m-8" onClick={handleSaveBook}>
+          Save
+        </button>
       </div>
-      <div className="my-4">
-        <label className="text-xl mr-4 text-gray-500">Author</label>
-        <input
-          type="text"
-          value={author}
-          onChange={(e) => setAuthor(e.target.value)}
-          className="border-2 border-gray-500 px-4 py-2 w-full"
-        />
-      </div>
-      <div className="my-4">
-        <label className="text-xl mr-4 text-gray-500">Publish year</label>
-        <input
-          type="text"
-          value={publishYear}
-          onChange={(e) => setPublishYear(e.target.value)}
-          className="border-2 border-gray-500 px-4 py-2 w-full"
-        />
-      </div>
-      <button className="p-2 bg-sky-300 m-8" onClick={handleSaveBook}>
-        Save
-      </button>
-    </div>
     </div>
   );
 };
--- frontend/src/pages/DeleteBook.jsx
+++ frontend/src/pages/DeleteBook.jsx
@@ -3,11 +3,13 @@ import BackButton from "../components/BackButton";
 import Spinner from "../components/Spinner";
 import axios from "axios";
 import { useNavigate, useParams } from "react-router-dom";
+import { useSnackbar } from "notistack";
 
 const DeleteBook = () => {
   const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
   const { id } = useParams();
+  const { enqueueSnackbar } = useSnackbar();
 
   const handleDeleteBook = () => {
     setLoading(true);
@@ -15,11 +17,12 @@ const DeleteBook = () => {
       .delete(`http://localhost:5555/books/${id}`)
       .then(() => {
         setLoading(false);
+        enqueueSnackbar("Book Deleted successfully", { variant: "success" });
         navigate("/");
       })
       .catch((error) => {
         setLoading(false);
-        alert("Error, please check console");
+        enqueueSnackbar("Error", { variant: "error" });
         console.log(error);
       });
   };
@@ -30,12 +33,13 @@ const DeleteBook = () => {
       <h1 className="text-3xl my-4">Delete Book</h1>
       {loading ? <Spinner /> : ""}
       <div className="flex flex-col items-center border-2 border-sky-400 rounded-xl w-[600px] p-8 mx-auto">
-        <h3 className="text-2xl">Are you sure you want to delete this book?</h3>
+        <h3 className="text-2xl">Are You Sure You want to delete this book?</h3>
+
         <button
           className="p-4 bg-red-600 text-white m-8 w-full"
           onClick={handleDeleteBook}
         >
-          Yes, Delete Book
+          Yes, Delete it
         </button>
       </div>
     </div>
