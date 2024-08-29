520276db2bd5d0ea90d5891dfce6e4a9cb7a55b6
Sdg877 <sdrakegill@gmail.com>
2024-08-29 13:58:38 +0100

    Snackbar added for create, edit and delete. Project completed

--- backend/index.js
+++ backend/index.js
@@ -10,11 +10,12 @@ const app = express();
 
 app.use(express.json());
 
-app.use(cors({
-  origin: 'http://localhost:3000',
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type'],
-}));
+app.use(cors());
+// app.use(cors({
+//   origin: 'http://localhost:3000',
+//   methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//   allowedHeaders: ['Content-Type'],
+// }));
 
 const DATABASE_URL = process.env.DATABASE_URL;
 const PORT = process.env.PORT || 5555;
